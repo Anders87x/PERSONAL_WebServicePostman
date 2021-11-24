451a7ac54b92680093d08830e89b333eaaa7949e
Anders.87 <davis_anderson_87@hotmail.com>
2021-11-23 21:32:15 -0500

    23/11/21 Subiendo a Github

--- config/conexion.php
+++ config/conexion.php
@@ -4,15 +4,20 @@
 
         protected function Conexion(){
             try {
-				$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=andercode_webservice","root","");
-				return $conectar;	
+                //QA
+				//$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=tuto_andercode_webservice","root","");
+                //Produccion Godaddy
+                //$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=apirestphpmysql","andercode1","@ndercode");
+                //Produccion Heroku
+                $conectar = $this->dbh = new PDO("mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_f08d006b9871cdc","b350610a2d9a9c","c8084c58");
+				return $conectar;
 			} catch (Exception $e) {
 				print "¡Error BD!: " . $e->getMessage() . "<br/>";
-				die();	
+				die();
 			}
         }
 
-        public function set_names(){	
+        public function set_names(){
 			return $this->dbh->query("SET NAMES 'utf8'");
         }
     }
